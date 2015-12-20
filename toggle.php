b459552889b4f9b6d56f88f461992601ef73bdac
jacobischwartz <jacob@schwambell.com>
2015-12-19 22:03:16 -0600

    Fix toggle, add output to indicate what happened.

--- toggle.php
+++ toggle.php
@@ -5,5 +5,9 @@ if(isset($_REQUEST['pin']) && (intval($_REQUEST['pin']) > 0)) {
   $pin = intval($_REQUEST['pin']);
 }
 
-exec( "gpio -g read " . $pin, $status );
-exec( 'gpio -g write ' . $pin . ' ' . !$status[0] );
+exec( "gpio -g read " . $pin, $status_before );
+system( "gpio -g mode " . $pin . " out" );
+system( 'gpio -g write ' . $pin . ' ' . ($status_before[0] ? 0 : 1) );
+exec( "gpio -g read " . $pin, $status_after );
+
+echo 'Pin ' . $pin . ' changed from ' . $status_before[0] . ' to ' . $status_after[0];
